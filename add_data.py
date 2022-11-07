926b1f85ae0f54cc8962a9e20d3db0dba83ba0af
maximbabaiev <maxim.babaiev92@gmail.com>
2022-11-07 20:15:46 +0200

    add func from data

--- add_data.py
+++ add_data.py
@@ -16,12 +16,31 @@ def addFriend(connection):
 
 
 def addPost(connection):
-    with connection.cursor as cursor:
+    with connection.cursor() as cursor:
         add_post = f"insert into `posts` (postName, Login) values ('{postName}', '{Login}')"
         cursor.execute(add_post)
         connection.commit()
 
 
+def editInfoFromUser(connection):
+    with connection.cursor() as cursor:
+        edit_user = f"update `users` set surname = '{surname}'"
+        cursor.execute(edit_user)
+        connection.commit()
+
+def findUser(connction):
+    find_user = f"select surname from `users` where surname = '{surname}'"
+    cursor.execute(find_user)
+    connction.commit()
+
+def selectAllFromUser(connection):
+    select_all_from_user = f"select * from `users` where name = '{name}'"
+    cursor.execute(select_all_from_user)
+    connection.commit()
+
+
 def delUser(connection):
     with connection.cursor() as cursor:
         del_user = f"delete from '{table}' where "
+        cursor.execute(del_user)
+        connection.commit()
